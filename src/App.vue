1a852b1065822b4db777d0a8fd28537d4614dadc
Daniel Blain <rpgzm@hotmail.com>
2020-07-06 01:07:12 -0700

    Added register button and stub page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <router-link to="/register">Register</router-link> |
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
---
+++ src/views/Register.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="register">
+    <h1>This is a registration page</h1>
+  </div>
+</template>
